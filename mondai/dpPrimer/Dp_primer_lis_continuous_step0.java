328947b3bc81582b2208337193239cb040f4ddc5
yuuta-uc <onlytext.official@gmail.com>
2023-12-12 23:23:49 +0900

    DPメニュー　最長増加連続部分列

---
+++ mondai/dpPrimer/Dp_primer_lis_continuous_step0.java
@@ -0,0 +1,29 @@
+//https://paiza.jp/works/mondai/dp_primer/dp_primer_lis_continuous_step0
+
+import java.util.*;
+
+public class dp_primer_lis_continuous_step0 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+       
+		int[] dp = new int[n];
+		dp[0] = 1;
+		for(int i = 1; i < n; i++) {
+			if(arr[i] >= arr[i-1]) {
+					dp[i] = dp[i-1]+1;
+			} else {
+					dp[i] = 1;
+			}  
+		}
+		
+		Arrays.sort(dp);
+		System.out.println(dp[n-1]);
+
+    }
+}
